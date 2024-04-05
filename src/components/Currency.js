a8f386a3971b7b78f2fc88088dd6ee13a5560ef0
psokhi <preetpal.sokhi@gmail.com>
2024-04-05 17:03:30 -0400

    dropdown menu white bg

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,27 +1,55 @@
 import React, { useState } from 'react';
 
-const CurrencyDropdown = () => {
-    const [selectCurrency, setSelectCurrency] = useState('£ Pound');
-	const [showMenu, setShowMenu] = useState(false);
-	
-    const handleClick = () => {setShowMenu(!showMenu)}
-	const handleChange = (event) => {
-        setSelectCurrency(event.target.value);
-		setShowMenu(false);
-    }	
-    return (
-    <div className='alert alert-secondary'>
-    <span onClick={handleClick}>Currency ({selectCurrency}) &#9660;</span>
-	{showMenu && (
-    <select className='alert alert-secondary' value={selectCurrency} onChange={handleChange}>
-        <option value="$ Dollar">Dollar</option>
-        <option value="£ Pound">Pound</option>
-        <option value="€ Euro">Euro</option>
-        <option value="₹ Ruppee">Ruppee</option>
-        </select>) 
-	}
+const CurrencyDropdown = ({ options, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectCurrency, setSelectCurrency] = useState('£ Pound');
+  const [warning,setWarning] = useState(false)
 
-</div>
-    );
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectCurrency(option);
+    setIsOpen(false);
+    // setWarning(true);
+  };
+
+  return (
+    <>
+      <button className="alert alert-secondary" onClick={toggleDropdown}>
+        Currency ({selectCurrency}) &#9660;
+      </button>
+      {isOpen && (
+        <div 
+        className="alert alert-secondary"
+        // onMouseEnter={()=>setWarning(true)} 
+        // onMouseLeave={()=>setWarning(false)}
+        >
+          {currencyList.map((currency) => (
+            <div 
+            onMouseEnter={()=>setWarning(true)}
+            onMouseLeave={()=>setWarning(false)}
+            >
+              <h6 
+              key={currency.index}
+              onClick={() => handleOptionClick(currency.option)}
+              style={
+                {backgroundColor: warning? 'white' : 'transparent'}}
+              >{currency.option}</h6></div>
+            
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
+
+let currencyList = [
+    {index:1, option: "$ Dollar"},
+    {index:2, option:"£ Pound"}, 
+    {index:3, option:"€ Euro"}, 
+    {index:4, option:"₹ Ruppee"}
+]
+
 export default CurrencyDropdown;
